e07cff88377fdfc6bae2555956f3b59d5efcbbea
Ibrahimsaidou23 <124426759+Ibrahimsaidou23@users.noreply.github.com>
2023-02-15 17:03:13 +0100

    Add files via upload

---
+++ oddWhile.js
@@ -0,0 +1,7 @@
+let i = 0 ;
+while (i<=19) {
+if( i % 2 === 1){
+  console.log(i);
+}  
+i++;
+}
---
+++ weekendVerifieds.js
@@ -0,0 +1,8 @@
+let day = "lundi";
+  if (day !=  "samedi" || day != "dimanche")
+  {
+    console.log("weekday");
+  }
+  else {
+    console.log("weekend");
+  }
